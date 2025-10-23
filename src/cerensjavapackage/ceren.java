1344bfa2a57eec9fdcc107f3e13bab475e98a850
Ceren Celayir <cerensunny2004@gmail.com>
2025-10-23 11:41:05 +0200

    ceren.java

---
+++ src/cerensjavapackage/ceren.java
@@ -0,0 +1,10 @@
+package cerensjavapackage;
+
+public class ceren {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/cerensjavapackage/cerensjavaclass.java
+++
@@ -1,5 +0,0 @@
-package cerensjavapackage;
-
-public class cerensjavaclass {
-
-}
